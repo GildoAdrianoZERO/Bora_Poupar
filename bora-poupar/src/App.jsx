2402677646cdf39eab457466e34fbd00cb5fc1f9
GILDO ADRIANO NORBERTO DA SILVA <133284362+GildoAdrianoZERO@users.noreply.github.com>
2026-01-27 10:54:42 -0300

    TASK 01: Setup do Ambiente e Lógica de Renderização de Lista

--- bora-poupar/src/App.jsx
+++ bora-poupar/src/App.jsx
@@ -2,14 +2,22 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const quadrados = Array(100).fill(0);
 
   return (
     <>
-      <div>
-        <h1>
-          Bora Poupar
-        </h1>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4"> 
+        <div className="grid grid-cols-10 gap-2">
+          {
+            quadrados.map((item, index) => (
+            <div key={index} className="w-10 h-10 bg-amber-600 border border-amber-800 rounded shadow-sm">
+
+            </div>
+
+          ))}
+          
+        </div>
       </div>
     </>
   )
